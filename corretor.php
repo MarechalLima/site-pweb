947d8369a7e084bee70c88e42891fc8b46a0c68d
Nicholas Lima de Souza Silva <nicholaslima.contato@outlook.com>
2017-11-20 15:11:36 -0300

    Bloquear acesso não autorizado

--- corretor.php
+++ corretor.php
@@ -4,6 +4,12 @@
     <link rel="stylesheet" type="text/css" href="css/master.css">
   </head>
   <body>
-    <?php include 'supmenu.php'; ?>
+    <?php include 'supmenu.php';
+    session_start();
+    if ($_SESSION['logado']==FALSE) {
+      echo "<script>alert('Usuário não logado!'); window.location = 'index.php'</script>";
+      exit();
+    }
+    ?>
   </body>
 </html>
