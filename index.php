92043096058ae0039bde932c155eee77506e7507
Unknown <eduardo.lucio.correia@gmail.com>
2017-11-20 16:56:32 -0300

    Material design!
    Instalação do material design e sua importação

--- index.php
+++ index.php
@@ -5,6 +5,14 @@
     <meta charset="utf-8">
     <link rel="icon" href="images/icon.png">
     <link rel="stylesheet" type="text/css" href="css/login.css">
+
+    <!--Import Google Icon Font-->
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <!--Import materialize.css-->
+    <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
+    <!--Let browser know website is optimized for mobile-->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+
   </head>
   <body>
     <?php
@@ -26,5 +34,9 @@
         <input type="submit" value="Enviar" class="inputButton">
       </div>
     </form>
+
+
+    <script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+    <script type="text/javascript" src="js/materialize.min.js"></script>
   </body>
 </html>
