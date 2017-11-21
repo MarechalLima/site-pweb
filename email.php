6b479368b3b9cd11d779d2959ad7d076a4dd6158
Nicholas Lima de Souza Silva <nicholaslima.contato@outlook.com>
2017-11-20 21:57:19 -0300

    Email PHP Funcional + salvamento em txt

--- email.php
+++ email.php
@@ -7,11 +7,29 @@
 
   echo "Email enviado!";
 
-  $to      = 'nicholas.aula@gmail.com';
+  $to      = $_POST['dest'];
   $subject = 'the subject';
-  $message = 'hello';
-  $headers = 'From: nicholas.aula@gmail.com' . "\r\n" .
-    'Reply-To: nicholas.aula@gmail.com' . "\r\n" .
-    'X-Mailer: PHP/' . phpversion();
-  mail($to, $subject, $message, $headers);
+  $message = $_POST['texto'];
+  $headers  = 'MIME-Version: 1.0' . "\r\n";
+  $headers .= 'Content-type: text/html; charset=utf-8' . "\r\n";
+  $headers .= 'From: nicholas.aula@gmail.com' . "\r\n" .
+    'Reply-To: '.$to . "\r\n";
+
+  if(!empty($message) && !empty($to)){
+    mail($to, $subject, $message, $headers);
+
+    $arquivo = file("historico.txt");
+
+    $n = count($arquivo);
+    $data = date("d/m/y");
+    $message = " ".$message;
+    $hora = date("H:i");
+    $arquivo[$n]=$data . "," . $hora . "," . trim($message) . "," . trim($to);
+    $arquivo[$n + 1] = "\n";
+    file_put_contents('historico.txt',implode($arquivo));
+    header('location: corretor.php');
+  }
+  else{
+    echo "<script>alert('Dados incompletos!'); window.location = 'corretor.php'</script>";
+  }
  ?>
