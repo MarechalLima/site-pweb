a6524f7ca0802cc4cdd5d8fd686936aea968d565
Luis Fillipe <fillipe2510@gmail.com>
2017-11-21 15:00:54 -0300

    Acho que Json foi feito

--- enviando.php
+++ enviando.php
@@ -2,17 +2,25 @@
 
   $texto = $_POST['texto'];
   $destinatario  =$_POST['dest'];
-  $arquivo = fopen("historico.json");
-
+  $arquivo = file("historico.json");
+  $aux=preg_split("/}/",$arquivo[0]);
+  for($i=0;$i<count($aux)-1;$i++) {
+    $aux[$i].='}';
+  }
   if(!empty($texto) && !empty($destinatario)){
-    $n = count($arquivo);
-
     $data = date("d/m/y");
     $hora = date("H:i");
 
-    $arquivo[$n]=array("data"=>$data,"hora"=>$hora,"mensagem"=>$texto,"destinatario"=>$destinatario);
-    $json_array = json_encode($arquivo);
-    file_put_contents('historico.json',$json_array);
+    $dados=array("data"=>$data,"hora"=>$hora,"mensagem"=>$texto,"destinatario"=>$destinatario);
+
+    $arquivo = Null;
+
+    for($i=0;$i<count($aux)-1;$i++) {
+      $arquivo[$i] = $aux[$i];
+    }
+    array_push($arquivo,json_encode($dados));
+    echo(json_decode($arquivo[0])->data);
+    file_put_contents('historico.json',$arquivo);
     header('location: corretor.php');
   }
   else{
