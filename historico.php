9bcc450d4ae828613f40530a8d85f71f6825580d
Unknown <eduardo.lucio.correia@gmail.com>
2017-11-21 10:30:28 -0300

    Collapsible no historico
    Melhorando view do historico de envios.

--- historico.php
+++ historico.php
@@ -12,48 +12,53 @@
     <?php include 'supmenu.php' ?>
 
     <div class="container">
+      <h4> Histórico de envios </h4>
+      <ul class="collapsible popout" data-collapsible="accordion">
 
-      <div class="row">
-        <h4>Histórico de envios</h4>
+        <?php
+          for($i=0;$i<count($arquivo);$i++){
+            $dados = preg_split("/,/",$arquivo[$i]);
+
+            $dia = $dados[0];
+            $hora = $dados[1];
+            $msg = $dados[2];
+            $dest = $dados[3];
+
+        ?>
+
+        <li>
+          <div class="collapsible-header" style="display: block">
+            <i class="material-icons left">email</i>
+            <div class="secondary-content"><?=$dia." às ". $hora?></div>
+            <h5 style="display: inline">Título</h5> <br>
+            <?="To: ".$dest?>
+
+
+          </div>
+
+          <div class="collapsible-body">
+            <span><?=$msg?></span>
+          </div>
+        </li>
+
+
+      <?php } ?>
+
+      </ul>
+      <div class="fixed-action-btn">
+        <a class="btn-floating btn-large waves-effect waves-light red right modal-trigger" href="corretor.php">
+          <i class="material-icons">create</i>
+        </a>
       </div>
-
-      <div class="row">
-        <table class="bordered striped">
-          <thead>
-            <tr>
-              <th> Dia </td>
-              <th> Horário </td>
-              <th> Mensagem </td>
-              <th> Destinatário </td>
-            </tr>
-          </thead>
-
-          <tbody>
-
-          <?php
-            for($i=0;$i<count($arquivo);$i++){
-              $dados = preg_split("/,/",$arquivo[$i]);
-          ?>
-            <tr>
-              <td> <?= $dados[0] ?> </td>
-              <td> <?= $dados[1] ?> </td>
-              <td> <?= $dados[2] ?> </td>
-              <td> <?= $dados[3] ?> </td>
-            </tr>
-          <?php
-            }
-          ?>
-
-          </tbody>
-        </table>
-      </div>
-
-      <div class="row">
-        <a class="btn waves-effect waves-light" href="corretor.php"> <i class="material-icons left">arrow_back</i> Voltar </a>
-      </div>
-
 
     </div>
+
+    <script type="text/javascript">
+      $(document).ready(function(){
+        $('.collapsible').collapsible();
+      });
+
+    </script>
 
   </body>
 </html>
