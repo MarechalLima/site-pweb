226c7372267091b81a45e7488d94803d00214d00
Nicholas Lima de Souza Silva <nicholaslima.contato@outlook.com>
2017-11-22 09:02:10 -0300

    Enviando.php deleted -> new file email.php

--- email.php
+++ email.php
@@ -1,35 +1,45 @@
 <?php
   session_start();
   if ($_SESSION['logado']==FALSE) {
-    echo "<script>alert('Usuário não logado!'); window.location = 'index.php'</script>";
-    exit();
+  echo "<script>alert('Usuário não logado!'); window.location = 'index.php'</script>";
+  exit();
   }
 
   echo "Email enviado!";
 
-  $to      = $_POST['dest'];
   $subject = 'the subject';
-  $message = $_POST['texto'];
   $headers  = 'MIME-Version: 1.0' . "\r\n";
   $headers .= 'Content-type: text/html; charset=utf-8' . "\r\n";
   $headers .= 'From: nicholas.aula@gmail.com' . "\r\n" .
-    'Reply-To: '.$to . "\r\n";
-  
-  /*if(!empty($message) && !empty($to)){
-    mail($to, $subject, $message, $headers);
+  'Reply-To: '.$to . "\r\n";
 
-    $arquivo = file("historico.txt");
+  $texto = $_POST['texto'];
+  $destinatario  =$_POST['dest'];
 
-    $n = count($arquivo);
+  $arquivo = file("historico.json");
+  $aux=preg_split("/}/",$arquivo[0]);
+  for($i=0;$i<count($aux)-1;$i++) {
+    $aux[$i].='}';
+  }
+  if(!empty($texto) && !empty($destinatario)){
+    mail($destinatario, $subject, $texto, $headers);
+
     $data = date("d/m/y");
-    $message = " ".$message;
     $hora = date("H:i");
-    $arquivo[$n]=$data . "," . $hora . "," . trim($message) . "," . trim($to);
-    $arquivo[$n + 1] = "\n";
-    file_put_contents('historico.txt',implode($arquivo));
+
+    $dados=array("data"=>$data,"hora"=>$hora,"mensagem"=>$texto,"destinatario"=>$destinatario);
+
+    $arquivo = Null;
+
+    for($i=0;$i<count($aux)-1;$i++) {
+      $arquivo[$i] = $aux[$i];
+    }
+    array_push($arquivo,json_encode($dados));
+    file_put_contents('historico.json',$arquivo);
     header('location: corretor.php');
   }
   else{
     echo "<script>alert('Dados incompletos!'); window.location = 'corretor.php'</script>";
-  }*/
- ?>
+  }
+
+?>
