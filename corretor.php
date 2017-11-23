83463d691ab7515c0570f29f46bd6ab8e62e8343
Unknown <eduardo.lucio.correia@gmail.com>
2017-11-22 22:32:07 -0300

    Funções do corretor implementadas
    Todas as funções do corretor implementadas e funcionando.

--- corretor.php
+++ corretor.php
@@ -3,6 +3,7 @@
   <head>
     <link rel="stylesheet" type="text/css" href="css/master.css">
     <link rel="stylesheet" type="text/css" href="css/editor.css">
+    <link rel="stylesheet" type="text/css" href="css/colorPick.min.css">
     <script src="js/corretor.js">
     </script>
   </head>
@@ -34,7 +35,7 @@
         </div>
         <div class="input-field col s12">
 
-          <div id="editor-texto" contenteditable="true" class="grey lighten-2" style="padding:1em">
+          <div id="editor-texto" contenteditable="true" class="grey lighten-2" style="padding:2em">
 
           </div>
         </div>
@@ -52,22 +53,27 @@
           <div class="col s1">
             <button type="button" onclick="formatCode('removeFormat')" class="waves-effect waves-light btn-flat"><i class="material-icons">format_clear</i></button>
           </div>
+
           <div class="col s1">
-            <button type="button" class="waves-effect waves-light btn-flat"><i class="material-icons">text_fields</i></button>
+            <button type="button" class="waves-effect waves-light btn-flat dropdown-button"
+              data-activates="dropdown-size"><i class="material-icons">text_fields</i></button>
+          </div>
+
+          <div class="col s1">
+            <button type="button" onclick="formatCode('insertUnorderedList')" class="waves-effect waves-light btn-flat"><i class="material-icons">format_list_bulleted</i></button>
           </div>
           <div class="col s1">
-            <button type="button" class="waves-effect waves-light btn-flat"><i class="material-icons">format_list_bulleted</i></button>
+            <button type="button" onclick="formatCode('insertOrderedList')" class="waves-effect waves-light btn-flat"><i class="material-icons">format_list_numbered</i></button>
           </div>
           <div class="col s1">
-            <button type="button" class="waves-effect waves-light btn-flat"><i class="material-icons">format_list_numbered</i></button>
+            <button type="button" id="text-color" class="waves-effect waves-light btn-flat"><i class="material-icons">format_color_text</i></button>
           </div>
           <div class="col s1">
-            <button type="button" class="waves-effect waves-light btn-flat"><i class="material-icons">format_color_text</i></button>
-          </div>
-          <div class="col s1">
-            <button type="button" class="waves-effect waves-light btn-flat"><i class="material-icons">format_color_fill</i></button>
+            <button type="button" id="color-fill" class="waves-effect waves-light btn-flat"><i class="material-icons">format_color_fill</i></button>
           </div>
         </div>
+
+        <a href="#" class="color"></a>
 
       </div>
 
@@ -76,9 +82,17 @@
       <div class="row">
         <div class="input-field col s12">
           <input id="email" type="email" name="dest" class="validate">
-          <label for="email" data-error="Email inválido" data-success="Email válido">Email</label>
+          <label for="email" data-error="Email inválido" data-success="Email válido">Email    </label>
         </div>
       </div>
+
+      <!-- Dropdown do botao text_fields-->
+      <ul id='dropdown-size' class='dropdown-content'>
+        <li><a href="#" onclick="formatCode('fontSize',7)" style="font-size: 16pt;">Enorme</a></li>
+        <li><a href="#" onclick="formatCode('fontSize',5)" style="font-size: 12pt;">Grande</a></li>
+        <li><a href="#" onclick="formatCode('fontSize',3)" style="font-size: 10pt;">Médio</a></li>
+        <li><a href="#"onclick="formatCode('fontSize',1)" style="font-size: 8pt;">Pequeno</a></li>
+      </ul>
 
 
       <button class="btn waves-effect waves-light" type="submit" name="enviar">Enviar
@@ -87,6 +101,9 @@
       <a href="historico.php"  class="btn waves-effect waves-light orange"><i class="material-icons left">history</i> Ver histórico </a>
     </div>
 
-
+    <script src="js/colorPick.min.js"></script>
+    
   </body>
+
+
 </html>
--- js/corretor.js
+++ js/corretor.js
@@ -1,3 +1,21 @@
-function formatCode(command){
-  document.execCommand(command, 0, false);
+function formatCode (command, value) {
+  //document.getElementById("editor-texto").focus();
+  document.execCommand(command, false, value)
 }
+
+function setColor (color, cmd) {
+  document.execCommand('styleWithCSS', false, true);
+  document.execCommand(cmd, false, color)
+}
+
+$("#text-color").colorPick({
+  'onColorSelected': function() {
+    setColor(this.color, 'foreColor');
+  }
+});
+
+$("#color-fill").colorPick({
+  'onColorSelected': function() {
+    setColor(this.color, 'hiliteColor');
+  }
+});
