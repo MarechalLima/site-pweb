136834484491c7fcd3a37c1a4030c96514e676b9
Vinicyus <vinicyus346@gmail.com>
2017-11-26 13:20:12 -0300

    Backend cadastro usuario
    git mais mandão q o Estado

---
+++ cadastrando.php
@@ -0,0 +1,33 @@
+<?php
+  session_start();
+
+  $usuario = $_POST["usuario"];
+  $senha = $_POST["senhacad"];
+  $senhaconf = $_POST["senhacadconf"];
+
+  $arquivo = file("cadastro.json");
+
+  if(empty($arquivo)) {
+
+  } else {
+
+  }
+
+  if(isset($usuario) && isset($senha) && isset($senhaconf)) {
+
+    if($senha == $senhaconf) {      
+      $dados = array('usuario' => $usuario, 'senha' => $senha);
+      $dados = (array) $dados;
+
+      file_put_contents('cadastro.json',json_encode($arquivo));
+      header('location: index.php');
+
+      echo "<script> alert('Cadastrado! :-)'); window.location = 'index.php' </script>";
+    } else {
+      echo "<script> alert('Senhas diferentes!'); window.location = 'index.php' </script>";
+    }
+
+  } else {
+    echo "<script> alert('Dados incompletos!'); window.location = 'index.php' </script>";
+  }
+?>
