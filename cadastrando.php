8a9f4e0672ab9310cf8c6896798b8d7acae15997
Vinicyus <vinicyus346@gmail.com>
2017-11-26 17:46:45 -0300

    cadastro agora verificando se ja existe usuario com esse nome

--- cadastrando.php
+++ cadastrando.php
@@ -15,25 +15,43 @@
     $arquivo = json_decode($arquivo,TRUE);
   }
 
+  $count = count($arquivo);
+  $aux1 = false;
+
   if(isset($usuario) && isset($senha) && isset($senhaconf)) {
 
-    if($senha == $senhaconf) {
-      $dados = array($usuario => $senha);
-      $dados = (array) $dados;
+    for($i = 0; $i < $count; $i++) {
+      $teste1 = (string) key($arquivo[$i]);
+      $teste2 = (string) $usuario;
 
-      array_push($arquivo, $dados);
-      file_put_contents('cadastro.json',json_encode($arquivo));
-
-      //print_r($arquivo[3]["Joao"]);
-
-      echo "<script> alert('Cadastrado! :-)'); window.location = 'index.php' </script>";
-
-      header('location: index.php');
-
-    } else {
-      echo "<script> alert('Senhas diferentes!'); window.location = 'index.php' </script>";
+      if(strtolower($teste1) == strtolower($teste2)) {
+        $aux1 = true;
+        break;
+      } else {
+        $aux1 = false;
+      }
     }
 
+    if($aux1 == true) {
+       echo "<script> alert('Usuário já cadastrado!'); window.location = 'index.php' </script>";
+    } else {
+      if($senha == $senhaconf) {
+        $dados = array($usuario => $senha);
+        $dados = (array) $dados;
+
+        array_push($arquivo, $dados);
+
+        file_put_contents('cadastro.json', json_encode($arquivo));
+
+        print_r($arquivo[0]);
+
+        echo "<script> alert('Cadastrado! :-)'); window.location = 'index.php' </script>";
+
+        header('location: index.php');
+      } else {
+        echo "<script> alert('Senhas diferentes!'); window.location = 'index.php' </script>";
+      }
+    }
   } else {
     echo "<script> alert('Dados incompletos!'); window.location = 'index.php' </script>";
   }
