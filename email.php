543cbee903328cf0276fab1834dd84eb99265ccd
Nicholas Lima de Souza Silva <nicholaslima.contato@outlook.com>
2017-11-27 20:03:51 -0300

    Alteração no email para incluir titulo no envio.

--- email.php
+++ email.php
@@ -7,7 +7,7 @@
 
   echo "Email enviado!";
 
-  $subject = 'Email suspeito';
+  $subject = $_POST['titulo'];
   $headers  = 'MIME-Version: 1.0' . "\r\n";
   $headers .= 'Content-type: text/html; charset=utf-8' . "\r\n";
   $headers .= 'From: nicholas.aula@gmail.com' . "\r\n";
