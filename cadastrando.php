b6f06db6886a3cfd281f9b4fdc190e8ac31e07fe
Unknown <eduardo.lucio.correia@gmail.com>
2017-11-28 09:31:53 -0300

    Identificando usuarios ja cadastrados

--- cadastrando.php
+++ cadastrando.php
@@ -15,29 +15,46 @@
     $arquivo = json_decode($arquivo,TRUE);
   }
 
+  $count = count($arquivo);
+  $aux1 = false;
+
   if(isset($usuario) && isset($senha) && isset($senhaconf) && (!empty($usuario)) && (!empty($senha)) && (!empty($senhaconf))) {
+    for($i = 0; $i < $count; $i++) {
+     $teste1 = (string) key($arquivo[$i]);
+     $teste2 = (string) $usuario;
+     if(strtolower($teste1) == strtolower($teste2)) {
+        $aux1 = true;
+        break;
+      } else {
+        $aux1 = false;
+      }
+    }
 
-    if($senha == $senhaconf) {
-      $dados = array($usuario => $senha);
-      $dados = (array) $dados;
-
-      array_push($arquivo, $dados);
-      file_put_contents('cadastro.json',json_encode($arquivo));
-
-      //print_r($arquivo[3]["Joao"]);
-
-     // echo "<script> alert('Cadastrado! :-)'); window.location = 'index.php' </script>";
-
-      header('location: index.php?SignUp=TRUE');
-
+    if($aux1 == true) {
+      echo "<script> alert('Usuário já cadastrado!'); window.location = 'index.php' </script>";
     } else {
+      if($senha == $senhaconf) {
+        $dados = array($usuario => $senha);
+        $dados = (array) $dados;
 
-      header('location: index.php?DifferentPasswords=TRUE');      
-      //echo "<script> alert('Senhas diferentes!'); window.location = 'index.php' </script>";
+        array_push($arquivo, $dados);
+        file_put_contents('cadastro.json',json_encode($arquivo));
+
+        //print_r($arquivo[3]["Joao"]);
+
+       // echo "<script> alert('Cadastrado! :-)'); window.location = 'index.php' </script>";
+
+        header('location: index.php?SignUp=TRUE');
+
+      } else {
+
+        header('location: index.php?DifferentPasswords=TRUE');
+        //echo "<script> alert('Senhas diferentes!'); window.location = 'index.php' </script>";
+      }
     }
 
   } else {
-    header('location: index.php?IncompleteData=TRUE');    
+    header('location: index.php?IncompleteData=TRUE');
     //echo "<script> alert('Dados incompletos!'); window.location = 'index.php' </script>";
   }
 
